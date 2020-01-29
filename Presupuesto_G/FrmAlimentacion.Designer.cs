300d6c0e1368ef31c4d27345c06662f2020607e8
carlitosSoporte <59172826+carlitosSoporte@users.noreply.github.com>
2020-01-29 11:33:51 -0500

    se organiza frm alimentaciòn

--- Presupuesto_G/FrmAlimentacion.Designer.cs
+++ Presupuesto_G/FrmAlimentacion.Designer.cs
@@ -35,123 +35,118 @@
             this.txtCantidad = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.txtValor = new System.Windows.Forms.TextBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.txtFecha = new System.Windows.Forms.TextBox();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.txtHora = new System.Windows.Forms.TextBox();
             this.btnGuardar = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // txtNombre
             // 
-            this.txtNombre.Location = new System.Drawing.Point(135, 60);
+            this.txtNombre.Location = new System.Drawing.Point(101, 162);
             this.txtNombre.Name = "txtNombre";
-            this.txtNombre.Size = new System.Drawing.Size(236, 34);
+            this.txtNombre.Size = new System.Drawing.Size(276, 29);
             this.txtNombre.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 65);
+            this.label1.Location = new System.Drawing.Point(12, 165);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(103, 29);
+            this.label1.Size = new System.Drawing.Size(83, 22);
             this.label1.TabIndex = 1;
             this.label1.Text = "Nombre";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(13, 137);
+            this.label2.Location = new System.Drawing.Point(383, 165);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(116, 29);
+            this.label2.Size = new System.Drawing.Size(93, 22);
             this.label2.TabIndex = 2;
             this.label2.Text = "Cantidad";
             // 
             // txtCantidad
             // 
-            this.txtCantidad.Location = new System.Drawing.Point(135, 132);
+            this.txtCantidad.Location = new System.Drawing.Point(482, 162);
             this.txtCantidad.Name = "txtCantidad";
-            this.txtCantidad.Size = new System.Drawing.Size(236, 34);
+            this.txtCantidad.Size = new System.Drawing.Size(80, 29);
             this.txtCantidad.TabIndex = 3;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(13, 205);
+            this.label3.Location = new System.Drawing.Point(590, 165);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(72, 29);
+            this.label3.Size = new System.Drawing.Size(58, 22);
             this.label3.TabIndex = 4;
             this.label3.Text = "Valor";
             // 
             // txtValor
             // 
-            this.txtValor.Location = new System.Drawing.Point(135, 205);
+            this.txtValor.Location = new System.Drawing.Point(654, 162);
             this.txtValor.Name = "txtValor";
-            this.txtValor.Size = new System.Drawing.Size(236, 34);
+            this.txtValor.Size = new System.Drawing.Size(164, 29);
             this.txtValor.TabIndex = 5;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(18, 285);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(82, 29);
-            this.label4.TabIndex = 6;
-            this.label4.Text = "Fecha";
-            // 
-            // txtFecha
-            // 
-            this.txtFecha.Location = new System.Drawing.Point(135, 280);
-            this.txtFecha.Name = "txtFecha";
-            this.txtFecha.Size = new System.Drawing.Size(236, 34);
-            this.txtFecha.TabIndex = 7;
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(405, 280);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(26, 29);
-            this.label5.TabIndex = 8;
-            this.label5.Text = "y";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(463, 285);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(67, 29);
-            this.label6.TabIndex = 9;
-            this.label6.Text = "Hora";
-            // 
-            // txtHora
-            // 
-            this.txtHora.Location = new System.Drawing.Point(577, 275);
-            this.txtHora.Name = "txtHora";
-            this.txtHora.Size = new System.Drawing.Size(236, 34);
-            this.txtHora.TabIndex = 10;
             // 
             // btnGuardar
             // 
-            this.btnGuardar.Location = new System.Drawing.Point(23, 628);
+            this.btnGuardar.Location = new System.Drawing.Point(833, 153);
             this.btnGuardar.Name = "btnGuardar";
             this.btnGuardar.Size = new System.Drawing.Size(126, 44);
             this.btnGuardar.TabIndex = 11;
             this.btnGuardar.Text = "Guardar";
             this.btnGuardar.UseVisualStyleBackColor = true;
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(28, 239);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(790, 333);
+            this.dataGridView1.TabIndex = 12;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(407, 64);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(155, 22);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "ALIMENTACIÒN";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(833, 240);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 44);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Modificar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(833, 290);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(126, 44);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "Eliminar";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // FrmAlimentacion
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 27F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1400, 759);
-            this.Controls.Add(this.btnGuardar);
-            this.Controls.Add(this.txtHora);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.txtFecha);
+            this.ClientSize = new System.Drawing.Size(994, 596);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label4);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnGuardar);
             this.Controls.Add(this.txtValor);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.txtCantidad);
@@ -160,10 +155,12 @@
             this.Controls.Add(this.txtNombre);
             this.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Margin = new System.Windows.Forms.Padding(5);
             this.MaximizeBox = false;
             this.Name = "FrmAlimentacion";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FrmAlimentacion";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -177,11 +174,10 @@
         private System.Windows.Forms.TextBox txtCantidad;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox txtValor;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox txtFecha;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.TextBox txtHora;
         private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
